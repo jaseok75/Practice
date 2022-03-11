85f032d228fc48afe7fa592525f3879e9c99c048
jaseok75 <74055323+jaseok75@users.noreply.github.com>
2022-03-11 17:36:33 +0900

    202203111736 commit

--- Baekjoon/15649_N과 M(1).py
+++ Baekjoon/15649_N과 M(1).py
@@ -6,6 +6,9 @@ Comment:
 - Silver 3
 """
 
+"""
+첫 번째 방법
+"""
 import itertools
 
 if __name__ == "__main__":
@@ -16,3 +19,32 @@ if __name__ == "__main__":
         for j in range(len(nCr[i])):
             print(nCr[i][j], end=' ')
         print()
+
+"""
+두 번째 방법
+"""
+"""
+Project: 백준_15649_N과 M(1)
+Date: 2022.03.11.금.
+Comment:
+- 백트래킹
+- 쉬울줄 알았는데 너무 어려운걸
+-  join은 좋은 친구
+"""
+
+
+def solution():
+    if len(num) == m:
+        print(' '.join(map(str, num)))
+
+    for i in range(1, n + 1):
+        if i not in num:
+            num.append(i)
+            solution()
+            num.pop()
+
+
+if __name__ == "__main__":
+    n, m = map(int, input().split())
+    num = []
+    solution()
