38ce3dac3806119eb5c965e241957fe7d17a3a27
jaseok75 <74055323+jaseok75@users.noreply.github.com>
2022-03-12 11:10:40 +0900

    202203121110 commit

---
+++ Baekjoon/15650_N과 M(2).py
@@ -0,0 +1,25 @@
+"""
+Project: 백준_15650_N과 M(2)
+Date: 2022.03.12.토.
+Comment:
+- 백트래킹
+"""
+
+
+def solution():
+    if len(num) == m:
+        print(' '.join(map(str, num)))
+        return
+
+    for i in range(1, n + 1):
+        if i not in num:
+            if (len(num) < 1) or (i > num[len(num) - 1]):
+                num.append(i)
+                solution()
+                num.pop()
+
+
+if __name__ == "__main__":
+    n, m = map(int, input().split())
+    num = []
+    solution()
