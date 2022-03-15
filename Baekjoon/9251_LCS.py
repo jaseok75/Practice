9df084a9e7072a30b70e307c97b88dcac36ecc39
jaseok75 <74055323+jaseok75@users.noreply.github.com>
2022-03-15 14:19:33 +0900

    2022.3151419 commit

--- Baekjoon/9251_LCS.py
+++ Baekjoon/9251_LCS.py
@@ -3,7 +3,6 @@ Project: 백준_9251_LCS
 Date: 2022.03.15.화.
 Comment:
 - 동적 계획법
-- 이것도 메모리 제한 좀 있네
 """
 
 if __name__ == "__main__":
@@ -13,11 +12,24 @@ if __name__ == "__main__":
     len_B = len(string_B) + 1
     LCS = [[0] * len_A for _ in range(len_B)]
 
+    # 최장 공통 부분수열 길이 구하기
     for i in range(1, len_B):
         for j in range(1, len_A):
             if string_A[j - 1] == string_B[i - 1]:
                 LCS[i][j] = LCS[i - 1][j - 1] + 1
             else:
                 LCS[i][j] = max(LCS[i - 1][j], LCS[i][j - 1])
+    print(LCS[len_B - 1][len_A - 1])
 
-    print(max(LCS[len_B - 1]))
+    # 최장 공통 부분수열 찾기
+    i, j = len_B - 1, len_A - 1
+    result = ''
+    while LCS[i][j] != 0:
+        if LCS[i][j] == LCS[i - 1][j]:
+            i -= 1
+        elif LCS[i][j] == LCS[i][j - 1]:
+            j -= 1
+        else:
+            result += string_A[j - 1]
+            i, j = i - 1, j - 1
+    print(''.join(reversed(result)))
